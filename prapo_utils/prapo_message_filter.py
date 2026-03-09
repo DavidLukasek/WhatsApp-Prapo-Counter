159105f52d2df60989aaff3a0fc8f7c2beb64fae
David Lukášek <lukasda7@fel.cvut.cz>
2026-03-09 16:25:54 +0100

    Expanded word filter to include 'pražského' and 'povstání' strings

--- prapo_utils/prapo_message_filter.py
+++ prapo_utils/prapo_message_filter.py
@@ -21,16 +21,18 @@ def filterMessages(messages, filterCode):
     	Filters messages that:
     	1) contain the user-provided filterCode
     	AND
-    	2.1) contain 'Prapo' in some approximate form
+    	2.1) contain 'prapo', 'pražského' or 'povstání'
+             in some approximate form
 	    OR
-	    2.2) contain <Média vynechány> since that includes Prapo-counting stickers and photos
+	    2.2) contain <Média vynechány> / (file attached) since
+             that includes Prapo-counting stickers and photos
     """
     filterCode = filterCode.lower()
 
     filtered = []
     for message in messages:
         msg_lower = message.lower()
-        if filterCode in msg_lower and (is_similar_to(msg_lower, "prapo") or is_similar_to(msg_lower, "<Média vynechány>") or is_similar_to(msg_lower, "(file attached)")):
+        if filterCode in msg_lower and (is_similar_to(msg_lower, "prapo") or is_similar_to(msg_lower, "<Média vynechány>") or is_similar_to(msg_lower, "(file attached)") or is_similar_to(msg_lower, "pražského") or is_similar_to(msg_lower, "povstání")):
             filtered.append(message)
 
     return filtered
